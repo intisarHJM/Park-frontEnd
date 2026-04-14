e314801c2517c749a8a91a9f46584fe0f7c78ef3
Intisar Hussain Jaafar <Intesarabdullah67@gmail.com>
2026-04-14 18:37:15 +0300

    Home page rendering rides added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css"
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+// import ViewRide from "./pages/ViewRide"
+// import TicketForm from "./pages/TicketForm"
+import Nav from "./components/Nav"
 
 function App() {
-  return <main></main>
+  return (
+    <>
+      <h1 id="main-title">Central Landing Park</h1>
+      <Nav />
+      <Routes>
+        <Route path="/rides" element={<Home />} />
+        {/* <Route path="/rides/:id" element={<ViewRide />} />
+      <Route path="/rides/:rideId/tickets/new" element={<TicketForm />} /> */}
+        {/* <Route path="/rides/:id" element={<ViewTicket />} /> */}
+      </Routes>
+    </>
+  )
 }
 
 export default App
