cf91726e72bebce17318f845140e7da27e848ab8
Intisar Hussain Jaafar <Intesarabdullah67@gmail.com>
2026-04-15 15:17:11 +0300

    ticket delete button added in the form

--- src/components/RideCard.jsx
+++ src/components/RideCard.jsx
@@ -4,11 +4,11 @@ import { useNavigate } from "react-router-dom"
 const RideCard = ({ name, imageURL, type, tickets }) => {
   const navigate = useNavigate()
 
+
   return (
     <div className="rideCard">
       <h3>{name}</h3>
       <img src={imageURL} alt={name} />
-      <h3>type: {type}</h3>
 
       <ul id="tickets">
         {tickets.map((ticket) => (
@@ -24,6 +24,7 @@ const RideCard = ({ name, imageURL, type, tickets }) => {
       <button onClick={() => navigate(`/rides/${ride._id}/${ride.tickets}`)}>
         view details
       </button>
+
     </div>
   )
 }
