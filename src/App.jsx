c620527f1d37a0a6ad5b233217729fae8d14acc6
Aboood105 <orcs882@gmail.com>
2026-04-15 10:29:23 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
 import "./App.css"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import { useState } from 'react'
-import TicketForm from './pages/TicketForm'
-import TicketCard from './components/TicketCard'
+import { useState } from "react"
+import TicketForm from "./pages/TicketForm"
+import TicketCard from "./components/TicketCard"
 // import ViewRide from "./pages/ViewRide"
 // import TicketForm from "./pages/TicketForm"
+import btn from "./components/btn.jsx"
 
 function App() {
-
-  const [tickets, setTickets] = useState([])
   return (
     <>
       <h1 id="main-title">Central Landing Park</h1>
-
-
-
-       <TicketForm tickets={tickets} setTickets={setTickets} />
-
-
-      <TicketCard tickets={tickets} />
+      <br />
+      <btn />
       <Routes>
+        {/* <Route path="/" element={<Btn />} /> */}
         <Route path="/rides" element={<Home />} />
         {/* <Route path="/rides/:id" element={<ViewRide />} />
       <Route path="/rides/:rideId/tickets/new" element={<TicketForm />} /> */}
